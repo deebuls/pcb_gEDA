d338fc70aaf50d0550d7b843fd0eb4304ea01bee
Deebul Nair <deebul.nair@smail.inf.h-brs.de>
2015-11-07 14:27:04 +0100

    Adding schematic and pcb for e stop board

---
+++ mir_e_stop_board/mir_e_stop.cmd
@@ -0,0 +1,79 @@
+# Pin name action command file
+
+# Start of element CONN108
+ChangePinName(CONN108, 1, 1)
+ChangePinName(CONN108, 2, 2)
+
+# Start of element CONN107
+ChangePinName(CONN107, 1, 1)
+ChangePinName(CONN107, 2, 2)
+
+# Start of element CONN106
+ChangePinName(CONN106, 1, 1)
+ChangePinName(CONN106, 2, 2)
+
+# Start of element CONN105
+ChangePinName(CONN105, 1, 1)
+ChangePinName(CONN105, 2, 2)
+
+# Start of element CONN104
+ChangePinName(CONN104, 1, 1)
+ChangePinName(CONN104, 2, 2)
+
+# Start of element CONN103
+ChangePinName(CONN103, 1, 1)
+ChangePinName(CONN103, 2, 2)
+
+# Start of element J103
+ChangePinName(J103, 1, 1)
+ChangePinName(J103, 2, 2)
+
+# Start of element J102
+ChangePinName(J102, 1, 1)
+ChangePinName(J102, 2, 2)
+
+# Start of element D103
+ChangePinName(D103, 1, 1)
+ChangePinName(D103, 2, 2)
+
+# Start of element D102
+ChangePinName(D102, 1, 1)
+ChangePinName(D102, 2, 2)
+
+# Start of element U103
+ChangePinName(U103, 2, 2)
+ChangePinName(U103, 6, 6)
+ChangePinName(U103, 4, 4)
+ChangePinName(U103, 1, 1)
+ChangePinName(U103, 8, 8)
+
+# Start of element U102
+ChangePinName(U102, 2, 2)
+ChangePinName(U102, 6, 6)
+ChangePinName(U102, 4, 4)
+ChangePinName(U102, 1, 1)
+ChangePinName(U102, 8, 8)
+
+# Start of element CONN102
+ChangePinName(CONN102, 3, 3)
+ChangePinName(CONN102, 1, 1)
+ChangePinName(CONN102, 2, 2)
+
+# Start of element J101
+ChangePinName(J101, 1, 1)
+ChangePinName(J101, 2, 2)
+
+# Start of element D101
+ChangePinName(D101, 1, 1)
+ChangePinName(D101, 2, 2)
+
+# Start of element U101
+ChangePinName(U101, 2, 2)
+ChangePinName(U101, 6, 6)
+ChangePinName(U101, 4, 4)
+ChangePinName(U101, 1, 1)
+ChangePinName(U101, 8, 8)
+
+# Start of element CONN101
+ChangePinName(CONN101, 1, 1)
+ChangePinName(CONN101, 2, 2)
